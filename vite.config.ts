e32583a85745c80e5a76fe0d7ce095903e826953
vivacreating2 <vivacreating2@gmail.com>
2026-02-18 14:09:46 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: './',
+        base: '/sandraterapeuta---site/',
         
       server: {
         port: 3000,
