739468d34066c9773aed29b908496149cee87caf
vivacreating2 <vivacreating2@gmail.com>
2026-02-18 15:37:21 -0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: './',
-        
+        base: '/sandratereapeuta---site/',
       server: {
         port: 3000,
         host: '0.0.0.0',
